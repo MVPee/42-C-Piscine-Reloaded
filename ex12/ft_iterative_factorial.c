6c8428cecbaa456739da2ac77478788b4bbdb9d0
MVPee <mariusvanpee@hotmail.be>
2023-10-10 20:40:21 +0200

    small correction

--- ex12/ft_iterative_factorial.c
+++ ex12/ft_iterative_factorial.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 12:44:14 by mvan-pee          #+#    #+#             */
-/*   Updated: 2023/10/10 12:47:02 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2023/10/10 20:38:12 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@ int	ft_iterative_factorial(int nb)
 	int	result;
 	int	i;
 
-	if (nb < 0)
-		return (0);
-	result = 1;
 	i = 1;
+	result = 1;
+	if ((nb < 0) || (nb > 12))
+		return (0);
+	if (nb == 1 || nb == 0)
+		return (1);
 	while (i <= nb)
 		result *= i++;
 	return (result);
--- ex13/ft_recursive_factorial.c
+++ ex13/ft_recursive_factorial.c
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mvan-pee <mvan-pee@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mvpee <mvpee@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 12:47:37 by mvan-pee          #+#    #+#             */
-/*   Updated: 2023/10/10 12:48:21 by mvan-pee         ###   ########.fr       */
+/*   Updated: 2023/10/10 20:19:53 by mvpee            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
+	if (nb < 0 || (nb > 12))
 		return (0);
 	if (nb == 0 || nb == 1)
 		return (1);
